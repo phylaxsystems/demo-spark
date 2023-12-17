f8c00f51363b827e358bc2ca3e2667e33a969a3c
Odysseas.eth <odyslam@gmail.com>
2023-12-17 14:03:41 +0200

    feat: assets health alert

---
+++ alerts/assets-liability/AssetsLiability.t.sol
@@ -0,0 +1,80 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+
+import {Alert} from "phylax-std/Alert.sol";
+
+contract AssetsLiability is Alert {
+    address $healthcheckAddress = 0xfda082e00EF89185d9DB7E5DcD8c5505070F5A3B;
+    uint256 mainnet;
+    uint256 immutable PRECISION = 1e18;
+
+    function setUp() public {
+        mainnet = enableChain("mainnet");
+    }
+
+    address[] unhealthyReserves;
+
+    function test_assetsLiability() public chain(mainnet) {
+        address reserve;
+        uint256 liabilities;
+        uint256 assets;
+        IHealthchecks.ReserveAssetLiability[]
+            memory assetsAndLiabilities = IHealthchecks($healthcheckAddress)
+                .getAllReservesAssetLiability();
+        uint256 len = assetsAndLiabilities.length;
+        for (uint256 i; i < len; i++) {
+            reserve = assetsAndLiabilities[i].reserve;
+            liabilities = assetsAndLiabilities[i].liabilities;
+            assets = assetsAndLiabilities[i].assets;
+            exportData(reserve, liabilities, assets);
+            if (
+                liabilities > assets || assets - liabilities < 1000 * PRECISION
+            ) {
+                unhealthyReserves.push(reserve);
+            }
+        }
+        if (unhealthyReserves.length > 0) {
+            string memory reserves = "";
+            for (uint i; i < unhealthyReserves.length; i++) {
+                reserves = string.concat(
+                    reserves,
+                    "'",
+                    vm.toString(unhealthyReserves[i]),
+                    "'"
+                );
+                if (i != unhealthyReserves.length - 1) {
+                    reserves = string.concat(reserves, ", ");
+                }
+            }
+            string memory err = string.concat(
+                "The reserves [",
+                reserves,
+                "] are not healthy"
+            );
+            revert(err);
+        }
+    }
+
+    function exportData(
+        address reserve,
+        uint256 liabilities,
+        uint256 assets
+    ) internal {
+        string memory key = string.concat("health-", vm.toString(reserve));
+        int256 diff = int256(assets) - int256(liabilities);
+        ph.export(key, vm.toString(diff));
+    }
+}
+
+interface IHealthchecks {
+    struct ReserveAssetLiability {
+        address reserve;
+        uint256 assets;
+        uint256 liabilities;
+    }
+
+    function getAllReservesAssetLiability()
+        external
+        view
+        returns (ReserveAssetLiability[] memory);
+}

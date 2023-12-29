512cc4503a4a62194eefb9c422cd4c356dfb0a4a
Odysseas.eth <odyslam@gmail.com>
2023-12-30 00:04:33 +0200

    fix: DAI limits; add dynamic alerts

--- alerts/assets-liability/AssetsLiability.t.sol
+++ alerts/assets-liability/AssetsLiability.t.sol
@@ -5,6 +5,7 @@ import {Alert} from "phylax-std/Alert.sol";
 
 contract AssetsLiability is Alert {
     address $healthcheckAddress = 0xfda082e00EF89185d9DB7E5DcD8c5505070F5A3B;
+    address $DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
     uint256 mainnet;
     uint256 immutable PRECISION = 1e18;
 
@@ -18,32 +19,58 @@ contract AssetsLiability is Alert {
         address reserve;
         uint256 liabilities;
         uint256 assets;
-        IHealthchecks.ReserveAssetLiability[] memory assetsAndLiabilities =
-            IHealthchecks($healthcheckAddress).getAllReservesAssetLiability();
+        IHealthchecks.ReserveAssetLiability[]
+            memory assetsAndLiabilities = IHealthchecks($healthcheckAddress)
+                .getAllReservesAssetLiability();
         uint256 len = assetsAndLiabilities.length;
         for (uint256 i; i < len; i++) {
             reserve = assetsAndLiabilities[i].reserve;
             liabilities = assetsAndLiabilities[i].liabilities;
             assets = assetsAndLiabilities[i].assets;
             exportData(reserve, liabilities, assets);
-            if (liabilities > assets || assets - liabilities > 1000 * PRECISION) {
-                unhealthyReserves.push(reserve);
+            if (reserve == $DAI) {
+                if (
+                    (liabilities > assets ||
+                        assets - liabilities > 1_000 * PRECISION)
+                ) {
+                    unhealthyReserves.push(reserve);
+                }
+            } else {
+                if (
+                    (liabilities > assets ||
+                        assets - liabilities > 3_000 * PRECISION)
+                ) {
+                    unhealthyReserves.push(reserve);
+                }
             }
         }
         if (unhealthyReserves.length > 0) {
             string memory reserves = "";
             for (uint256 i; i < unhealthyReserves.length; i++) {
-                reserves = string.concat(reserves, "'", vm.toString(unhealthyReserves[i]), "'");
+                reserves = string.concat(
+                    reserves,
+                    "'",
+                    vm.toString(unhealthyReserves[i]),
+                    "'"
+                );
                 if (i != unhealthyReserves.length - 1) {
                     reserves = string.concat(reserves, ", ");
                 }
             }
-            string memory err = string.concat("The reserves [", reserves, "] are not healthy");
+            string memory err = string.concat(
+                "The reserves [",
+                reserves,
+                "] are not healthy"
+            );
             revert(err);
         }
     }
 
-    function exportData(address reserve, uint256 liabilities, uint256 assets) internal {
+    function exportData(
+        address reserve,
+        uint256 liabilities,
+        uint256 assets
+    ) internal {
         string memory key = string.concat("health-", vm.toString(reserve));
         int256 diff = int256(assets) - int256(liabilities);
         ph.export(key, vm.toString(diff));
@@ -57,5 +84,8 @@ interface IHealthchecks {
         uint256 liabilities;
     }
 
-    function getAllReservesAssetLiability() external view returns (ReserveAssetLiability[] memory);
+    function getAllReservesAssetLiability()
+        external
+        view
+        returns (ReserveAssetLiability[] memory);
 }
